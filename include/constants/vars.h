41da49f641fa5ca6cb73b5724c5d77c83abccf8d
Kaitlyn <katykat5099@gmail.com>
2023-11-22 15:07:54 -0600

    OricorioFamily Pt2
    The dad of the family joins the pack

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -271,7 +271,7 @@
 #define VAR_UNUSED_0x40FB                                0x40FB // Unused Var
 #define VAR_UNUSED_0x40FC                                0x40FC // Unused Var
 #define VAR_UNUSED_0x40FD                                0x40FD // Unused Var
-#define VAR_UNUSED_0x40FE                                0x40FE // Unused Var
+#define VAR_ALOLAN_MAN_SAD                               0x40FE
 #define VAR_GNARLED_DEN                                  0x40FF
 
 #define VARS_END                                         0x40FF
